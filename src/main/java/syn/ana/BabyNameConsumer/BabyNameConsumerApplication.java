d74224fec4c29d62e937539d7f9bc398bcd68ab9
WareJosephB <warejosephb@gmail.com>
2018-12-04 11:13:01 +0000

    First commit

---
+++ src/main/java/syn/ana/BabyNameConsumer/BabyNameConsumerApplication.java
@@ -0,0 +1,12 @@
+package syn.ana.BabyNameConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyNameConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyNameConsumerApplication.class, args);
+	}
+}
---
+++ src/test/java/syn/ana/BabyNameConsumer/BabyNameConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package syn.ana.BabyNameConsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BabyNameConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
